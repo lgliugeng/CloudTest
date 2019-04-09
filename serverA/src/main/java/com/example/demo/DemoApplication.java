656f80241060049c1bb71273f646daeefc5ec905
lgliugeng <lg_liu217@163.com>
2019-04-09 16:48:20 +0800

    添加TestGit类

--- serverA/src/main/java/com/example/demo/DemoApplication.java
+++ serverA/src/main/java/com/example/demo/DemoApplication.java
@@ -21,6 +21,6 @@ public class DemoApplication {
 
     @RequestMapping("/serverA")
     public String serverA(String info){
-        return "This is " + info + " port:" + port + "";
+        return "This is " + info + " port:" + port + "111";
     }
 }
