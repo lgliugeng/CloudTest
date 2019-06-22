bb0f91d7ded57adacd3462cf634a7c287b1c46b7
liugeng <lg_liu217@163.com>
2019-06-22 16:44:21 +0800

    priorityQueue优先级队列demo

---
+++ cloud/cloudTest/src/main/java/com/liugeng/cloud/study/queue/PriorityQueueDemo.java
@@ -0,0 +1,74 @@
+package com.liugeng.cloud.study.queue;
+
+import java.text.DecimalFormat;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+
+public class PriorityQueueDemo {
+
+    public static void main(String[] args) {
+        //由大到小排序距离
+        Comparator<PointComparator> distanctCopmarator = new Comparator<PointComparator>() {
+            @Override
+            public int compare(PointComparator o1, PointComparator o2) {
+                if(o1.distance < o2.distance){
+                    return 1;
+                }
+                if(o1.distance > o2.distance){
+                    return -1;
+                }
+                return 0;
+            }
+        };
+
+        Queue<PointComparator> priorityQueue = new PriorityQueue<>(10,distanctCopmarator);
+        for (int i = 0;i < 8;i++){
+            Random r = new Random(2);
+            int ir = r.nextInt(10);
+            Point p1 = new Point(i* (ir+1)+1,i*(ir+2)+2);
+            Point p2 = new Point(i*(ir+3)+3,i*(ir+4)+4);
+            PointComparator pointComparator = new PointComparator(p1,p2);
+            priorityQueue.add(pointComparator);
+        }
+        DecimalFormat df = new DecimalFormat("#.##");
+        for (int i = 0;i < 8;i++){
+            System.out.println(df.format(priorityQueue.poll().distance));
+        }
+    }
+
+}
+
+//定义一个点
+class Point{
+    double x;
+
+    double y;
+
+    public Point(double x,double y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+//计算距离
+class PointComparator{
+
+    Point onePoint;
+
+    Point twoPoint;
+
+    public double distance;
+
+    public PointComparator(Point onePoint,Point twoPoint){
+        this.onePoint = onePoint;
+        this.twoPoint = twoPoint;
+        computeDistance();
+    }
+
+    private void computeDistance(){
+        double val = Math.pow((this.onePoint.x - this.twoPoint.x),2) + Math.pow((this.onePoint.y - this.twoPoint.y),2);
+        this.distance = Math.sqrt(val);
+    }
+}
