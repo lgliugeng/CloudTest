ff45ca88781a342f0176243d66016b51c868c67a
liugeng <lg_217liu@163.com>
2019-09-18 17:50:20 +0800

    线程中断测试

---
+++ cloud/cloudTest/src/main/java/com/liugeng/cloud/study/thread/InterruptTest.java
@@ -0,0 +1,48 @@
+package com.liugeng.cloud.study.thread;
+
+public class InterruptTest {
+
+    private volatile static  Boolean interrupt = false;
+
+    public static void main(String[] args)throws Exception {
+
+        Thread t1 = new Thread(new BusyThread());
+        t1.setName("busy线程");
+        Thread t2 = new Thread(new SleepThread());
+        t2.setName("sleep线程");
+        t1.start();
+        t2.start();
+        Thread.sleep(5);
+        //判断中断标识
+        interrupt = true;
+        //将运行中的线程修改中断状态，若不加的话，阻塞中的线程会无法中断，sleep中的1仍会打印，加的话将会抛出异常，中断后续的操作
+        t1.interrupt();
+        t2.interrupt();
+    }
+
+    static class BusyThread implements Runnable{
+        @Override
+        public void run() {
+            while (!interrupt) {
+                System.out.println(Thread.currentThread().getName() + "当前状态" + Thread.currentThread().isInterrupted());
+            }
+            System.out.println(Thread.currentThread().getName() + "当前状态" + Thread.currentThread().isInterrupted());
+        }
+    }
+
+    static class SleepThread implements Runnable{
+        @Override
+        public void run() {
+            while (!interrupt) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + "睡眠五秒……当前状态" + Thread.currentThread().isInterrupted());
+                    Thread.sleep(30);
+                    System.out.println(1);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + "当前状态" + Thread.currentThread().isInterrupted());
+        }
+    }
+}
