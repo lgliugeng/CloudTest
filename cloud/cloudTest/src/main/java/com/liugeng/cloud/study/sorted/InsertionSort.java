0717749cf5bc0623bf1e5942591894cd3d452b01
Dell <lg_liu217@163.com>
2020-04-27 17:29:37 +0800

    排序

---
+++ cloud/cloudTest/src/main/java/com/liugeng/cloud/study/sorted/InsertionSort.java
@@ -0,0 +1,23 @@
+package com.liugeng.cloud.study.sorted;
+
+/**
+ * 插入排序（将每个未排序的元素挨个比较）
+ */
+public class InsertionSort {
+
+    public static int[] insertionSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        int current;
+        for (int i = 0; i < array.length - 1; i++) {
+            current = array[i + 1];
+            int preIndex = i;
+            while (preIndex >= 0 && current < array[preIndex]) {
+                array[preIndex + 1] = array[preIndex];
+                preIndex--;
+            }
+            array[preIndex + 1] = current;
+        }
+        return array;
+    }
+}
---
+++ cloud/cloudTest/src/main/java/com/liugeng/cloud/study/sorted/ShellSort.java
@@ -0,0 +1,25 @@
+package com.liugeng.cloud.study.sorted;
+
+/**
+ * 希尔排序（缩小增量持续分组）
+ */
+public class ShellSort {
+
+    public static int[] shellSort(int[] array) {
+        int len = array.length;
+        int temp, gap = len / 2;
+        while (gap > 0) {
+            for (int i = gap; i < len; i++) {
+                temp = array[i];
+                int preIndex = i - gap;
+                while (preIndex >= 0 && array[preIndex] > temp) {
+                    array[preIndex + gap] = array[preIndex];
+                    preIndex -= gap;
+                }
+                array[preIndex + gap] = temp;
+            }
+            gap /= 2;
+        }
+        return array;
+    }
+}
